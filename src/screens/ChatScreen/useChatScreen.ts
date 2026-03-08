6da488f6e52a62e22d68a5a59990cca075f9bb17
Juste-Leo <leonard.adamo66@gmail.com>
2026-03-08 11:24:37 +0100

    correction

--- src/screens/ChatScreen/useChatScreen.ts
+++ src/screens/ChatScreen/useChatScreen.ts
@@ -121,7 +121,7 @@ export const useChatScreen = () => {
     const { conversationId, projectId } = route.params || {};
     if (conversationId) { setActiveConversation(conversationId); }
     else if (activeModelId) { createConversation(activeModelId, undefined, projectId); }
-    else if (activeImageModelId) { createConversation('', undefined, projectId); }
+    else if (activeImageModelId) { createConversation(null, undefined, projectId); }
   }, [route.params?.conversationId, route.params?.projectId, activeModelId, activeImageModelId, createConversation, setActiveConversation]);
 
   useEffect(() => {
